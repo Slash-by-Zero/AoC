380d996046474b3eee6d8cb49c060bb951fd6d73
/by0-laptop <qu.soldner@gmail.com>
2022-12-02 08:44:30 +0100

    Day 2 solved, Day 3 c file added changed some things regarding .gitignore

--- sbz/2022/02/02.c
+++ sbz/2022/02/02.c
@@ -1,3 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define newline ('\n'-'A')
+
 int main(int argc, char *argv[]){
-		return 0;
+	int res1=0, res2=0;
+	int opponent, you;
+	
+	for(;;){
+		opponent = fgetc(stdin) - 'A';
+		if(opponent == newline) break;
+		fgetc(stdin);
+		you=fgetc(stdin)-'X';
+		fgetc(stdin);
+		res1 += you + 1 + 3 * ((you - opponent + 4)%3);
+		res2 += you * 3 + 1 +  (you + opponent + 2)%3;
+	}
+	
+	printf("Part 1: %d\nPart 2: %d\n", res1, res2);
 }
