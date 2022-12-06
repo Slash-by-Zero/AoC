4a98add7f0aceea20228e987d420ea1779989522
/by0-laptop <qu.soldner@gmail.com>
2022-12-06 20:13:39 +0100

    Syncing with pc

--- sbz/2022/06/06.c
+++ sbz/2022/06/06.c
@@ -5,48 +5,46 @@
 
 int main(int argc, char *argv[]){
 	int res1 = 0, res2 = 0;
-	char c;
-	char marker[14];
-	memset(marker, 0, 4);
+	int c, i;
+	char contained[256];
+	int distinct;
+	char last[14];
 	
+	memset(contained, 0, 'z');
+	distinct = 0;
 	
-	for(c = fgetc(stdin);c!=EOF && c!='\n';c = fgetc(stdin)){
-		memcpy(marker+1, marker, 3);
-		marker[0] = c;
-		int equiv=0;
-		for(int i=0;i<3;i++){
-			for(int j=i+1;j<4;j++){
-				if(marker[i] == marker[j] || marker[j] == 0){
-					equiv = 1;
-					break;
-				}
+	for(c = fgetc(stdin);;c = fgetc(stdin)){
+		res1++;
+		memcpy(last+1, last, 3);
+		last[0] = c;
+		if(contained[c]){
+			for(i = 3; last[i] != c; i--){
+				contained[last[i]] = 0;
 			}
+			distinct -= i;
+			memset(last+i
 		}
-		
-		res1++; 
-		
-		if(!equiv) break;
-		
+		else{
+			contained[c] = 1;
+			if(++distinct == 4) break;
+		}
 	}
+	
 	
 	res2=res1;
 	
-	for(c = fgetc(stdin);c!=EOF && c!='\n';c = fgetc(stdin)){
-		memcpy(marker+1, marker, 13);
-		marker[0] = c;
-		int equiv=0;
-		for(int i=0;i<13;i++){
-			for(int j=i+1;j<14;j++){
-				if(marker[i] == marker[j] || marker[j] == 0){
-					equiv = 1;
-					break;
-				}
+	for(c = fgetc(stdin);;c = fgetc(stdin)){
+		res2++;
+		memcpy(last+1, last, 13);
+		last[0] = c;
+		if(contained[c]){
+			for(i = 13; last[i] != c; i--){
+				contained[last[i]] = 0;
 			}
+			distinct -= i;
 		}
-		
-		res2++; 
-		
-		if(!equiv) break;
+		else if(++distinct == 14) break;
+		else contained[c] = 1;
 	}
 	
 	printf("Part 1: %d\nPart 2: %d\n", res1, res2);
