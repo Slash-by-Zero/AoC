b880a324cbc050db68b3cc471bbbf28e021be49a
/by0-laptop <qu.soldner@gmail.com>
2022-12-08 14:02:07 +0100

    Improved Day 8 performance (not happy yet)

--- sbz/2022/08/08.c
+++ sbz/2022/08/08.c
@@ -7,9 +7,11 @@ int main(int argc, char *argv[]){
 	int res1=0, res2=0;
 	char line[128][128];
 	char vis[128][128];
-	int scores[128][128]; 
+	char top[128];
+	int bot[128][10];
 	memset(vis, 0, 128*128);
-	memset(scores, 0, 4*128*128);
+	memset(top, 0, 128);
+	memset(bot, 0, 4*128*10);
 	
 	for(int j=0;;j++){
 		if(!fgets(line[j], 128, stdin) || strlen(line[j]) <= 1){
@@ -20,42 +22,22 @@ int main(int argc, char *argv[]){
 	}
 	
 	for(int i = 0; line[i][0] != '\0';i++){
-		int left = '0'-1;
+		char left = '0'-1;
 		int right[10];
 		memset(right, 0, 40);
 		for(int j=0; line[i][j] != '\0'; j++){
+			if(line[i][j] > top[j]){
+				vis[i][j] = 1;
+				top[j] = line[i][j];
+			}
 			if(line[i][j] > left){
 				vis[i][j]=1;
 				left = line[i][j];
 			}
 			right[line[i][j] - '0'] = j;
 			memset(right, 0, 4 * (line[i][j]-'0'));
-		}
-		for(int j=0; j<10; j++){
-			vis[i][right[j]] = 1;
-		}
-	}
-	
-	for(int i = 0; line[0][i] != '\0';i++){
-		int top = '0'-1;
-		int bot[10];
-		memset(bot, 0, 40);
-		for(int j=0; line[j][i] != '\0'; j++){
-			if(line[j][i] > top){
-				vis[j][i]=1;
-				top = line[j][i];
-			}
-			bot[line[j][i] - '0'] = j;
-			memset(bot, 0, 4 * (line[j][i]-'0'));
-		}
-		for(int j=0; j<10; j++){
-			vis[bot[j]][i] = 1;
-		}
-	}
-	
-	for(int i=0; i<128; i++){
-		for(int j=0;j<128;j++){
-			res1+=vis[i][j];
+			bot[j][line[i][j] - '0'] = i;
+			memset(bot[j], 0, 4 * (line[i][j]-'0'));
 			
 			int score=1;
 			for(int k = 1;;k++){
@@ -98,8 +80,20 @@ int main(int argc, char *argv[]){
 					break;
 				}
 			}
-			scores[i][j] = score;
 			if(score > res2) res2 = score;
+		}
+		for(int j=0; j<10; j++){
+			vis[i][right[j]] = 1;
+		}
+	}
+	
+	for(int i = 0; i<128;i++){
+		for(int j=0; j<10; j++)
+		{
+			if(bot[i][j] != 0) vis[bot[i][j]][i] = 1;
+		}
+		for(int j=0;j<128;j++){
+			res1+=vis[j][i];
 		}
 	}
 	
