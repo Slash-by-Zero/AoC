81d2da73e235664d6d368bd8ce7a1b1ecd43f18b
/by0-laptop <qu.soldner@gmail.com>
2022-12-15 18:37:50 +0100

    Day 15 solved

--- sbz/2022/15/15.c
+++ sbz/2022/15/15.c
@@ -6,10 +6,7 @@
 
 #define MAX_LINE_LENGTH 128
 
-#define abs(c) (((c>0)-(c<0))*(c))
-
-#define row 2000000
-#define MAXX (row*2)
+#define abs(c) ((((c)>0)-((c)<0))*(c))
 
 struct sensor{
 	int x,y;
@@ -23,11 +20,23 @@ int main(int argc, char *argv[]){
 	long regions[11][2];
 	memset(regions, 0x00, sizeof(long)*22);
 	
-	struct sensor sensors[20];
+	struct sensor sensors[32];
 	int sensor_count=0;
 	
+	fgets(line, MAX_LINE_LENGTH, stdin);
+	int row=strtol(line, NULL, 10);
+	int MAXX=row*2;
 	
-	for(;;){
+	long plDiag[64];
+	memset(plDiag, 0, 64*sizeof(long));
+	int pld_count=1;
+	plDiag[0]=row;
+	long mnDiag[64];
+	memset(mnDiag, 0, 64*sizeof(long));
+	int mnd_count=1;
+	mnDiag[0]=0;
+	
+	for(;;sensor_count++){
 		if(!fgets(line, MAX_LINE_LENGTH, stdin) || strlen(line) <= 1) break;
 		
 		int xS, yS, xB, yB;
@@ -44,7 +53,40 @@ int main(int argc, char *argv[]){
 		
 		sensors[sensor_count].x=xS;
 		sensors[sensor_count].y=yS;
-		sensors[sensor_count++].dist=dist;
+		sensors[sensor_count].dist=dist;
+		
+		int tmp = xS + dist + 1 + yS;
+		for(int i=0;i<=pld_count;i++){
+			if(i==pld_count){
+				plDiag[pld_count++] = tmp;
+				break;
+			}
+			else if(plDiag[i] == tmp) break;
+		}
+		tmp = xS - dist - 1 + yS;
+		for(int i=0;i<=pld_count;i++){
+			if(i==pld_count){
+				plDiag[pld_count++] = tmp;
+				break;
+			}
+			else if(plDiag[i] == tmp) break;
+		}
+		tmp = xS + dist + 1 - yS;
+		for(int i=0;i<=mnd_count;i++){
+			if(i==mnd_count){
+				mnDiag[mnd_count++] = tmp;
+				break;
+			}
+			else if(mnDiag[i] == tmp) break;
+		}
+		tmp = xS - dist - 1 - yS;
+		for(int i=0;i<=mnd_count;i++){
+			if(i==mnd_count){
+				mnDiag[mnd_count++] = tmp;
+				break;
+			}
+			else if(mnDiag[i] == tmp) break;
+		}
 		
 		if(lower > xS) continue;
 		
@@ -63,21 +105,27 @@ int main(int argc, char *argv[]){
 		}
 	}
 	
-	for(int i=0;i<=MAXX;i++){
-		for(int j=0;j<=MAXX;j++){
+	for(int i=0;i<pld_count;i++){
+		for(int j=0;j<mnd_count;j++){
+			int tmp = plDiag[i]-mnDiag[j];
+			if(tmp%2 != 0) continue;
 			int p=1;
+			int y = tmp/2;
+			int x = mnDiag[j] + y;
+			if(x<0 || y<0 || x>MAXX || y>MAXX) continue;
 			for(int k=0;k<sensor_count;k++){
-				int dist = abs(sensors[k].x-i) + abs(sensors[k].y-j);
-				p &= dist > sensors[k].dist;
-				if(!p) break;
+				int dist = abs(sensors[k].x-x)+abs(sensors[k].y-y);
+				p = p && dist > sensors[k].dist;
 			}
-			if(p){
-				res2 = i*4000000+j;
-				goto part2f;
+			if(p)
+			{
+				res2 = x;
+				res2 *= 4000000l;
+				res2+= y;
+				printf("%d %d = %ld\n", x, y, ((long) x)*4000000+y);
 			}
 		}
 	}
-part2f:
 	
 	for(int i=0;i<10;i++){
 		if(regions[i][0] == 0 && regions[i][1] == 0){
