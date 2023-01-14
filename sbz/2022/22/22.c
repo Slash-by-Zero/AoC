10901d5a9471155820b8c8427296025cece8cc13
/by0 <qu.soldner@gmail.com>
2023-01-14 16:53:43 +0100

    A working algorithmic solution for day 22

--- sbz/2022/22/22.c
+++ sbz/2022/22/22.c
@@ -6,74 +6,77 @@
 
 #define MAX_LINE_LENGTH 256
 
-static int map[256][256];
+static long map[256][256];
 static int cubeSize = INT_MAX;
 static int move[4][2] = {{0,1},{1,0},{0,-1},{-1,0}};
-static int order[14] = {1,1,1,1,0,0,1,0,0,0,1,1,0,0};
-//static int order[14] = {1,1,1,0,0,1,1,1,0,0,0,0,1,0};
 
-static void calcEdges(){
-	int x=0, y=1, dir=0;
+#define OUT_OF_MAP(x, y) (map[y][x] < 0)
+
+struct side{
+	int x, y;
+	struct side *neighbors[4];
+} startingSide = {0}, *sides[6];
+
+static int sideCount = 0;
+
+static void createSide(struct side *this, int x, int y){
+	this->x = x;
+	this->y = y;
 	
-	int pos=0;
+	sides[sideCount++] = this;
 	
-	struct{
-		int x, y, dir;
-	} stack[7];
-	
-	for(;(map[y][x] & ~1) != 0;x++);
-	
-	for(int i=0;i<14;i++){
-		if(order[i]){
-			x += move[dir][1] * (cubeSize-1);
-			y += move[dir][0] * (cubeSize-1);
-			stack[pos].x=x;
-			stack[pos].y=y;
-			stack[pos].dir = (dir+2)%4;
-			pos++;
-		}
-		else{
-			pos--;
-			for(int j=0;j<cubeSize;j++){
-				int x2=stack[pos].x+move[stack[pos].dir][1]*j, y2=stack[pos].y+move[stack[pos].dir][0]*j;
-				int tmp = (x2) << 8 | (y2);
-				
-				map[y+move[(dir+3)%4][0]][x+move[(dir+3)%4][1]] &= (dir%2 == 0) ? (0xffff0000 | tmp) : (0x0000ffff | (tmp << 16));
-				
-				tmp = (x) << 8 | (y);
-				
-				map[y2+move[(stack[pos].dir+1)%4][0]][x2+move[(stack[pos].dir+1)%4][1]] &= (stack[pos].dir%2 == 0) ? (tmp | 0xffff0000) : (0x0000ffff | (tmp << 16));
-				
-				y+=move[dir][0];
-				x+=move[dir][1];
-			}
-			y-=move[dir][0];
-			x-=move[dir][1];
-		}
-		
-		if((map[y+move[(dir+3)%4][0]+move[dir][0]][x+move[(dir+3)%4][1]+move[dir][1]] & ~1) == 0){
-			x+=move[(dir+3)%4][1]+move[dir][1];
-			y+=move[(dir+3)%4][0]+move[dir][0];
-			dir = (dir+3)%4;
-		}
-		else if((map[y+move[dir][0]][x+move[dir][1]] & ~1) == 0){
-			x+=move[dir][1];
-			y+=move[dir][0];
-		}
-		else{
-			dir = (dir+1)%4;
+	for(int i=0;i<4;i++){
+		int newX = x+move[i][1]*cubeSize, newY = y+move[i][0]*cubeSize;
+		if(newX < 0 || newX >= 256 || newY < 0 || newY >= 256) continue;
+		if(!this->neighbors[i] && !OUT_OF_MAP(newX, newY)){
+			this->neighbors[i] = calloc(sizeof(**this->neighbors), 1);
+			this->neighbors[i]->neighbors[i ^ 2] = this;
+			
+			
+			
+			createSide(this->neighbors[i], newX, newY);
 		}
 	}
 }
 
+static int complete(struct side *this, struct side *a, struct side *b){
+	for(int i=0;i<4;i++){
+		if(a->neighbors[i] == this){
+			if(a->neighbors[(i-1) & 0x3]) return 0;
+			a->neighbors[(i-1) & 0x3] = b;
+			break;
+		}
+	}
+	for(int i=0;i<4;i++){
+		if(b->neighbors[i] == this){
+			b->neighbors[(i+1) & 0x3] = a;
+			break;
+		}
+	}
+	return 1;
+}
+
+static void calcNeighbors(struct side *this){
+	for(int i=0;i<4;i++){
+		if(this->neighbors[i] && this->neighbors[(i+1) & 0x3] && complete(this, this->neighbors[i], this->neighbors[(i+1) & 0x3])){
+			calcNeighbors(this->neighbors[i]);
+			calcNeighbors(this->neighbors[(i+1) & 0x3]);
+		}
+	}
+}
+
+static void calcCube(int x, int y){
+	createSide(&startingSide, x, y);
+	for(int i=0;i<sideCount;i++) calcNeighbors(sides[i]);
+}
+
 int main(int argc, char *argv[]){
-	long res1=0, res2=1;
+	long res1=0, res2=0;
 	char line[MAX_LINE_LENGTH];
 	
-	memset(map, 0xff, 256*256*sizeof(int));
+	memset(map, 0xff, 256*256*sizeof(**map));
 	
 	int row=1;
-	
 	
 	for(;;){
 		if(!fgets(line, MAX_LINE_LENGTH, stdin) || strlen(line)<=1) break;
@@ -94,12 +97,13 @@ int main(int argc, char *argv[]){
 	int x=1, y=1;
 	int dir=0;
 	
-	
-	calcEdges();
-	
 	for(x=1; map[y][x] != 0;x++);
 	
-	int x2=x, y2=y, dir2=0;
+	calcCube(x, y);
+	
+	fpos_t start_of_instructions;
+	
+	fgetpos(stdin, &start_of_instructions);
 	
 	for(c = fgetc(stdin); c != EOF && c != '\n'; c = fgetc(stdin)){
 		int n = c - '0';
@@ -107,79 +111,119 @@ int main(int argc, char *argv[]){
 			n = n*10 + c - '0';
 		}
 		
-		int prevx=x, prevy=y, prevdir;
+		int prevx=x, prevy=y;
 		for(int i=n;i>0; i--){
 			x += move[dir][1];
 			y += move[dir][0];
-			if((map[y][x] & ~1) != 0){
-				for(;(map[(y+256)%256][(x+256)%256] & ~1) != 0; x += (y += move[dir][0], move[dir][1]));
-				x=(x+256)%256;
-				y=(y+256)%256;
+			
+			if(OUT_OF_MAP(x,y)){
+				
+				for(;OUT_OF_MAP(x & 0xff, y & 0xff); x += (y += move[dir][0], move[dir][1]));
+				
+				x &= 0xff;
+				y &= 0xff;
 			}
+			
 			if(map[y][x] == 1){
 				x=prevx;
 				y=prevy;
 				break;
 			}
+			
 			prevx=x;
 			prevy=y;
 		}
-		
-		prevx=x2; prevy=y2; prevdir=dir2;
-		for(;n>0; n--){
-			x2 += move[dir2][1];
-			y2 += move[dir2][0];
-			if((map[y2][x2] & ~1) != 0){
-				if(prevx == 1 && prevy == 151){ 
-				}
-				int tmp = map[y2][x2];
-				if(dir2%2 != 0){
-					x2 = (0xff00 & tmp) >> 8;
-					y2 = (0xff & tmp) >> 0;
-				}
-				else{
-					x2 = (0xff000000 & tmp) >> 24;
-					y2 = (0xff0000 & tmp) >> 16;
-				}
-				if(map[y2][x2] == 1){
-					dir2=prevdir;
-					x2=prevx;
-					y2=prevy;
-					break;
-				}
-				tmp = prevx << 8 | prevy;
-				for(int i=0;i<4;i++){
-					int tmp2 = tmp;
-					if(i%2==0) tmp2 = tmp2 << 16;
-					tmp2 = ~tmp2 & map[y2-move[i][0]][x2-move[i][1]];
-					if((map[y2-move[i][0]][x2-move[i][1]] & ~1) != 0 && (tmp2 == -65536 || tmp2 == 65535)) {dir2 = i; break;}
-				}
-			}
-			else if(map[y2][x2] == 1){
-				x2=prevx;
-				y2=prevy;
-				break;
-			}
-			prevx=x2;
-			prevy=y2;
-			prevdir=dir2;
-		}
-		
 		
 		if(c == EOF || c == '\n') break;
 		
 		if(c == 'R') dir++;
 		if(c == 'L') dir--;
 		dir = (dir+4)%4;
-		if(c == 'R') dir2++;
-		if(c == 'L') dir2--;
-		dir2 = (dir2+4)%4;
 	}
 	
 	res1 = 1000*y+4*x+dir;
-	res2 = 1000*y2+4*x2+dir2;
 	
+	fsetpos(stdin, &start_of_instructions);
 	
+	x = 0;
+	y = 0;
+	dir = 0;
+	struct side *currSide = &startingSide;
+	
+	for(c = fgetc(stdin); c != EOF && c != '\n'; c = fgetc(stdin)){
+		int n = c - '0';
+		for(c = fgetc(stdin); c >= '0' && c <= '9'; c = fgetc(stdin)){
+			n = n*10 + c - '0';
+		}
+		
+		int prevx=x, prevy=y, prevdir = dir;
+		struct side *prevSide;
+		for(int i=n;i>0; i--){
+			x += move[dir][1];
+			y += move[dir][0];
+			
+			if(x >= cubeSize || y >= cubeSize || x < 0 || y < 0){
+				int off = 0;
+				switch(dir){
+					case(0):
+						off = y;
+						break;
+					case(1):
+						off = cubeSize - 1 - x;
+						break;
+					case(2):
+						off = cubeSize - 1 - y;
+						break;
+					case(3):
+						off = x;
+						break;
+				}
+				
+				currSide = currSide->neighbors[dir];
+				for(dir = 0;;dir++) if(currSide->neighbors[dir ^ 2] == prevSide) break;
+				
+				switch(dir){
+					case(0):
+						x = 0;
+						y = off;
+						break;
+					case(1):
+						y = 0;
+						x = cubeSize - 1 - off;
+						break;
+					case(2):
+						x = cubeSize - 1;
+						y = cubeSize - 1 - off;
+						break;
+					case(3):
+						y = cubeSize - 1;
+						x = off;
+						break;
+				}
+			}
+			
+			if(map[y + currSide->y][x + currSide->x] == 1){
+				x = prevx;
+				y = prevy;
+				dir = prevdir;
+				currSide = prevSide;
+				break;
+			}
+			
+			prevSide = currSide;
+			prevdir = dir;
+			prevx=x;
+			prevy=y;
+		}
+		
+		if(c == EOF || c == '\n') break;
+		
+		if(c == 'R') dir++;
+		if(c == 'L') dir--;
+		dir = (dir+4) & 0x3;
+	}
+	
+	res2 = 1000 * (y + currSide->y) + 4 * (x + currSide->x) + dir; 
 	
 	printf("Part 1: %ld\nPart 2: %ld\n", res1, res2);
 }
